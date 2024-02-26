c50dbbd0d16ca0b07f7ed940c7bbc9d02ed2bb03
Nike <nikesjolin@outlook.com>
2024-02-26 15:41:02 +0100

    Done with signupmodel

--- WebApp/Models/SignUpModel.cs
+++ WebApp/Models/SignUpModel.cs
@@ -1,11 +1,37 @@
-﻿namespace WebApp.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using WebApp.Helpers;
+
+namespace WebApp.Models;
 
 public class SignUpModel
 {
+    [Display(Name = "First name", Prompt = "Enter your first name", Order = 0)]
+    [Required(ErrorMessage = "Invalid first name")]
     public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Last name", Prompt = "Enter your last name", Order = 1)]
+    [Required(ErrorMessage = "Invalid last name")]
     public string LastName { get; set;} = null!;
+
+    [Display(Name = "Email address", Prompt = "Enter your email address", Order = 2)]
+    [DataType(DataType.EmailAddress)]
+    [Required(ErrorMessage = "Invalid email address")]
+    [RegularExpression("^[^@\\s]+@[^@\\s]+\\.[^@\\s]{2,}$", ErrorMessage = "Your email address is invalid")]
     public string Email { get; set; } = null!;
+
+    [Display(Name = "Password", Prompt = "Enter your password", Order = 3)]
+    [DataType(DataType.Password)]
+    [Required(ErrorMessage = "Invalid password")]
+    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", ErrorMessage = "Invalid password, must be a strong password")]
     public string Password { get; set; } = null!;
+
+    [Display(Name = "Confirm password", Prompt = "Confirm your password", Order = 4)]
+    [DataType(DataType.Password)]
+    [Required(ErrorMessage = "Password must be confirmed")]
+    [Compare(nameof(Password), ErrorMessage = "Password does not match")]
     public string ConfirmPassword { get; set; } = null!;
+
+    [Display(Name = "I agree to the Terms & Conditions.", Order = 5)]
+    [CheckBoxRequired(ErrorMessage = "You must accept the terms and conditions to proceed.")]
     public bool TermsAndConditions { get; set; } = false;
 }
