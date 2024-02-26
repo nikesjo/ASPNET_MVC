c5fa3269c75b3e4e068a3172f15a397d04987128
Nike <nikesjolin@outlook.com>
2024-02-26 16:48:48 +0100

    Added code for SignUp

--- WebApp/Controllers/AuthController.cs
+++ WebApp/Controllers/AuthController.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.ViewModels.Views;
 
 namespace WebApp.Controllers
 {
     public class AuthController : Controller
     {
         [Route("/signup")]
+        [HttpGet]
         public IActionResult SignUp()
         {
-            return View();
+            var viewModel = new SignUpViewModel();
+            return View(viewModel);
+        }
+
+        [Route("/signup")]
+        [HttpPost]
+        public IActionResult SignUp(SignUpViewModel viewmodel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewmodel);
+            }
+            return RedirectToAction("SignIn", "Auth");
         }
     }
 }
