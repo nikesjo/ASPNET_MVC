8b07210e1c5f29f249fe743421edbc9d1d2a5ef1
Nike <nikesjolin@outlook.com>
2024-02-26 15:16:08 +0100

    Added code in SignUpModel

--- WebApp/Models/SignUpModel.cs
+++ WebApp/Models/SignUpModel.cs
@@ -2,5 +2,10 @@
 
 public class SignUpModel
 {
-
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set;} = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string ConfirmPassword { get; set; } = null!;
+    public bool TermsAndConditions { get; set; } = false;
 }
