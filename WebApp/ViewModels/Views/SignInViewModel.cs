203b6ddcefa3e89149a31f19042de45a184baaf4
Nike <nikesjolin@outlook.com>
2024-02-26 17:19:33 +0100

    Added code for sign in

--- WebApp/ViewModels/Views/SignInViewModel.cs
+++ WebApp/ViewModels/Views/SignInViewModel.cs
@@ -6,4 +6,5 @@ public class SignInViewModel
 {
     public string Title { get; set; } = "Sign in";
     public SignInModel Form { get; set; } = new SignInModel();
+    public string? ErrorMessage { get; set; }
 }
