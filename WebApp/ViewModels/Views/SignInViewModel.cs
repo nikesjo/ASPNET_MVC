a99b8cd67ba20c9ccb289f762a7d1b96e3c1989e
Nike <nikesjolin@outlook.com>
2024-02-26 16:55:17 +0100

    Created viewmodel for signin

---
+++ WebApp/ViewModels/Views/SignInViewModel.cs
@@ -0,0 +1,9 @@
+﻿using WebApp.Models;
+
+namespace WebApp.ViewModels.Views;
+
+public class SignInViewModel
+{
+    public string Title { get; set; } = "Sign in";
+    public SignInModel Form { get; set; } = new SignInModel();
+}
