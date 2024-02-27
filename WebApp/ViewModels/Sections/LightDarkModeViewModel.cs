524912221e187e73254ee8f6dbf4162fe7fd9f1f
Nike <nikesjolin@outlook.com>
2024-02-27 10:26:32 +0100

    LightDarkMode section done

---
+++ WebApp/ViewModels/Sections/LightDarkModeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using WebApp.ViewModels.Components;
+
+namespace WebApp.ViewModels.Sections;
+
+public class LightDarkModeViewModel
+{
+    public string? Id { get; set; }
+    public ImageViewModel SliderButton { get; set; } = null!;
+    public string? TitleD { get; set; }
+    public ImageViewModel Dark { get; set; } = null!;
+    public string? TitleL { get; set; }
+    public ImageViewModel Light { get; set; } = null!;
+}
