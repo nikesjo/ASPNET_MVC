b5d6279e64a19ba687daa960e74f680e53fb66f9
Nike <nikesjolin@outlook.com>
2024-03-08 17:05:57 +0100

    Added new database and migrated entities

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -3,17 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRouting(x => x.LowercaseUrls = true);
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 
-//}
 
 var app = builder.Build();
 app.UseHsts();
