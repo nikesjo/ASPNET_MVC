7ff3ee5aaa34043b1241f621cfca156abf37500f
Nike <nikesjolin@outlook.com>
2024-03-08 16:55:24 +0100

    Configured DataContext

--- Infrastructure/Contexts/DataContext.cs
+++ Infrastructure/Contexts/DataContext.cs
@@ -1,10 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Contexts;
 
-public class DataContext : DbContext
+public class DataContext : IdentityDbContext<UserEntity>
 {
     public DataContext(DbContextOptions options) : base(options)
     {
+    }
+
+    public DbSet<AddressEntity> Addresses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<UserEntity>()
+            .HasMany(u => u.Addresses)
+            .WithOne(a => a.User)
+            .HasForeignKey(a => a.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
