8e326c72c4c6d7fff0b16c2f773c7e0692b3af80
Nike <nikesjolin@outlook.com>
2024-03-08 17:32:54 +0100

    Created UserSessionValidationMiddleware

--- Infrastructure/Helpers/Middlewares/UserSessionValidationMiddleware.cs
+++ Infrastructure/Helpers/Middlewares/UserSessionValidationMiddleware.cs
@@ -1,13 +1,31 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Infrastructure.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 
 namespace Infrastructure.Helpers.Middlewares;
 
-public class UserSessionValidationMiddleware
+public class UserSessionValidationMiddleware(RequestDelegate next)
 {
-    private readonly RequestDelegate _next;
+    private readonly RequestDelegate _next = next;
+    private static bool IsAjaxRequest(HttpRequest request) => request.Headers.XRequestedWith == "XMLHttpRequest";
 
-    public UserSessionValidationMiddleware(RequestDelegate next)
+    public async Task InvokeAsync(HttpContext context, UserManager<UserEntity> userManager, SignInManager<UserEntity> signInManager)
     {
-        _next = next;
+        if (context.User.Identity!.IsAuthenticated)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+            if (user == null)
+            {
+                await signInManager.SignOutAsync();
+
+                if (!IsAjaxRequest(context.Request) && context.Request.Method.Equals("GET", StringComparison.OrdinalIgnoreCase))
+                {
+                    var signInPath = "/signin";
+                    context.Response.Redirect(signInPath);
+                    return;
+                }
+            }
+        }
+        await _next(context);
     }
 }
