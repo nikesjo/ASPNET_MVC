bbab07a7d212f39da1cc9706b121f0bce886c354
Nike <nikesjolin@outlook.com>
2024-03-08 16:43:32 +0100

    Created UserEntity

---
+++ Infrastructure/Entities/UserEntity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Entities;
+
+public class UserEntity : IdentityUser
+{
+    [ProtectedPersonalData]
+    public string FirstName { get; set; } = null!;
+
+    [ProtectedPersonalData]
+    public string LastName { get; set;} = null!;
+
+    [ProtectedPersonalData]
+    public string? Bio { get; set; }
+
+    [ProtectedPersonalData]
+    public string? ProfileImageUrl { get; set; } = "/images/profile-image.svg";
+
+    public bool IsExternalAccount { get; set; } = false;
+}
