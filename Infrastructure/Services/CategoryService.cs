6687091ff1f51aaf06555a3811e4ae4d3b6a7381
Nike <nikesjolin@outlook.com>
2024-04-04 16:32:49 +0200

    Created CategoryService

---
+++ Infrastructure/Services/CategoryService.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Services;
+
+public class CategoryService(HttpClient http, IConfiguration configuration)
+{
+    private readonly HttpClient _http = http;
+    private readonly IConfiguration _configuration = configuration;
+
+    public async Task<IEnumerable<CategoryModel>> GetCategoriesAsync()
+    {
+        return null!;
+    }
+}
