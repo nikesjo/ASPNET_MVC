03453ac984854a783b994831b1eced5d52e714b6
Nike <nikesjolin@outlook.com>
2024-04-04 15:47:38 +0200

    Created AuthenticationConfiguration

---
+++ WebApp/Configurations/AuthenticationConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+
+namespace WebApp.Configurations;
+
+public static class AuthenticationConfiguration
+{
+    public static void RegisterAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDefaultIdentity<UserEntity>(x =>
+        {
+            x.User.RequireUniqueEmail = true;
+            x.SignIn.RequireConfirmedAccount = false;
+            x.Password.RequiredLength = 8;
+        }).AddEntityFrameworkStores<DataContext>();
+
+        services.ConfigureApplicationCookie(x =>
+        {
+            x.LoginPath = "/signin";
+            x.LogoutPath = "/signout";
+            x.AccessDeniedPath = "/denied";
+
+            x.Cookie.HttpOnly = true;
+            x.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            x.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+            x.SlidingExpiration = true;
+        });
+
+        services.AddAuthentication().AddFacebook(x =>
+        {
+            x.AppId = "1477856659826934";
+            x.AppSecret = "0ea4df4b97209bf7538c3e7f1bf12567";
+            x.Fields.Add("first_name");
+            x.Fields.Add("last_name");
+        });
+
+        services.AddAuthentication().AddGoogle(x =>
+        {
+            x.ClientId = "169070126063-bd5vkl5e8c4pd4a8mud5rvfiic10uv6p.apps.googleusercontent.com";
+            x.ClientSecret = "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";
+        });
+    }
+}
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,16 +1,14 @@
-using Infrastructure.Contexts;
-using Infrastructure.Entities;
 using Infrastructure.Helpers.Middlewares;
 using Infrastructure.Services;
 using WebApp.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRouting(x => x.LowercaseUrls = true);
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.RegisterDbContexts(builder.Configuration);
+
 builder.Services.AddHttpClient();
+builder.Services.RegisterDbContexts(builder.Configuration);
+builder.Services.RegisterAuthentication(builder.Configuration);
 builder.Services.AddSession(x =>
 {
     x.IdleTimeout = TimeSpan.FromMinutes(20);
@@ -18,38 +16,6 @@ builder.Services.AddSession(x =>
     x.Cookie.HttpOnly = true;
 });
 
-builder.Services.AddDefaultIdentity<UserEntity>(x =>
-{
-    x.User.RequireUniqueEmail = true;
-    x.SignIn.RequireConfirmedAccount = false;
-    x.Password.RequiredLength = 8;
-}).AddEntityFrameworkStores<DataContext>();
-
-builder.Services.ConfigureApplicationCookie(x =>
-{
-    x.LoginPath = "/signin";
-    x.LogoutPath = "/signout";
-    x.AccessDeniedPath = "/denied";
-
-    x.Cookie.HttpOnly = true;
-    x.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-    x.ExpireTimeSpan = TimeSpan.FromMinutes(60);
-    x.SlidingExpiration = true;
-});
-
-builder.Services.AddAuthentication().AddFacebook(x =>
-{
-    x.AppId = "1477856659826934";
-    x.AppSecret = "0ea4df4b97209bf7538c3e7f1bf12567";
-    x.Fields.Add("first_name");
-    x.Fields.Add("last_name");
-});
-
-builder.Services.AddAuthentication().AddGoogle(x =>
-{
-    x.ClientId = "169070126063-bd5vkl5e8c4pd4a8mud5rvfiic10uv6p.apps.googleusercontent.com";
-    x.ClientSecret = "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";
-});
 
 builder.Services.AddScoped<AddressManager>();
 
